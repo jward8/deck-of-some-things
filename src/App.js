8f3eb3d2a42738670c054f165e8891299ec804ed
jackmichael <jward8@wisc.edu>
2021-01-17 15:28:00 -0600

    added cards and functionality w/ some aesthetics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <h1 class="title">Deck of Some Things</h1>
-        <div class="card-container">
-          <Card></Card>
-        </div>
-      </body>
+      <h1 className="title">Deck of Some Things</h1>
+      <div>
+        <Card></Card>
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,50 @@
 import React from 'react';
 
+var cardTypes = ["Vizier", "Sun", "Moon", "Star", "Comet",
+"The Fates", "Throne", "Key", "Knight", "Gem", "Talons",
+"The Void", "Flames", "Skull", "Idiot", "Donjon", "Ruin",
+"Euryale", "Rogue", "Balance", "Fool", "Jester"];
+
 class Card extends React.Component {
     constructor(props){
         super(props);
         this.fullDeck = this.fullDeck.bind(this);
     }
 
+    getRandomInt(max){
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
     fullDeck(){
         var rows = [];
         var row = [];
+        let cards = JSON.parse(JSON.stringify(cardTypes));
         for(var i = 1; i <= 22; i++){
+            let num = this.getRandomInt(cards.length);
+            let card = cards[num];
+            cards.splice(num, 1);
             row.push(
-                <div key={i} class="card-body"></div>
+                <div className="card">
+                    <div className="card-back">
+                        <div key={i} className="card-body"></div>
+                    </div>
+                    <div className="card-front">
+                        <div key={card} className="card-type">
+                            <h2 className="card-type-text">{card}</h2>
+                        </div>
+                    </div>
+                </div>
             );
-            if(i  % 5 === 0){
-                rows.push(row);
-                row = [];
-            }
         }
+
+        rows.push(row);
 
         return rows;
     }
 
     render() {
         return(
-            <div class="card-container">{this.fullDeck()}</div>
+            <div className="card-container">{this.fullDeck()}</div>
         );
     }
 }
