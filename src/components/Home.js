bcb4c9c1a9ae3434226b8cc7c8adef5fbd0d8bb8
jackmichael <jward8@wisc.edu>
2021-02-07 22:04:18 -0600

    worked more on radio buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,15 @@ class Home extends React.Component{
             fullDeck: true,
             cardsToPull: 0
         }
+
+        this.deckInput = this.deckInput.bind(this);
+    }
+
+    deckInput(deckNum){
+        this.setState({deckSize: deckNum});
+        if(deckNum !== 22){
+            //FIXME
+        }
     }
 
     render(){
@@ -18,12 +27,19 @@ class Home extends React.Component{
                 <div className='home_options'>
                     <h1 className='home_title'>What Deck?</h1>
                     <form>
-                        <label className='full-deck'>
-                            <span className='full-deck-input'>
-                                <input type='radio' name='full'/>
-                                <span className='full-control'></span>
+                        <label className='deck-option'>Full
+                            <span className='full_control'>
+                                <input type='radio' checked='checked' name='radio'/>
+                                <span className='checkmark'></span>
                             </span>
-                            <span className='full-label'>Full</span>
+                        </label>
+                        <label className='deck-option'>
+                            <span className='full_control'>
+                                <input type='radio' name='radio'>
+                                    <input type='text' onChange={this.deckInput}/>
+                                </input>
+                                <span className='checkmark'></span>
+                            </span>
                         </label>
                     </form>
                 </div>
